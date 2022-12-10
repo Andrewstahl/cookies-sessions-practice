b7152c6ea1be21046c06b20c1a695ddb36ab4973
Andrew Stahl <andrewstahl96@gmail.com>
2022-12-10 14:23:40 -0500

    Changed default security settings in ActionController to prevent issues with fetch requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include ActionController::Cookies
 
+  # Added this to prevent default Cross-Site REquest Forgery (CSRF)
+  # security measures from blocking my API request. Read more 
+  # about that here - 
+  # https://guides.rubyonrails.org/security.html#cross-site-request-forgery-csrf
+  # UNNECESSARY IF YOU DO rails new appname --api WHEN CREATING YOUR APP
+  protect_from_forgery with: :null_session
+
 end
